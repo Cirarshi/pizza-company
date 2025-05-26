3ac6b362b87a96c3df933b335a9ce88b4b033a44
Utkarsh Kapoor <98.utkarsh.kapoor@gmail.com>
2025-05-26 10:43:10 +0530

    v3
    updated pizza images

--- src/index.js
+++ src/index.js
@@ -4,48 +4,54 @@ import "./index.css";
 import Header from "./Header";
 import Menu from "./Menu";
 import Footer from "./Footer";
+import cornImg from "./pizzas/Corn.jpg";
+import fresh from "./pizzas/fresh.JPG";
+import toasedBread from "./pizzas/ToastedBread.jpg";
+import spinachPizza from "./pizzas/spinach.jpg";
+import oniPizza from "./pizzas/onion.jpg";
+import evePizza from "./pizzas/evergreen.jpg";
 
 export const pizzaData = [
   {
     name: "Garden Fresh",
     ingredients: "Lots of mouth watering fresh veggies",
     price: 199,
-    photoName: "pizzas/fresh.JPG",
+    photoName: fresh,
     soldOut: false,
   },
   {
     name: "Toasted Bread",
     ingredients: "Toasted bread with veggies and cheese for a great start",
     price: 99,
-    photoName: "pizzas/ToastedBread.jpg",
+    photoName: toasedBread,
     soldOut: false,
   },
   {
     name: "Pizza Spinach",
     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 159,
-    photoName: "pizzas/spinach.jpg",
+    photoName: spinachPizza,
     soldOut: false,
   },
   {
     name: "Corn & Onion",
     ingredients: "A twist of a bit of sweet with corns",
     price: 129,
-    photoName: "pizzas/onion.jpg",
+    photoName: oniPizza,
     soldOut: false,
   },
   {
     name: "Pizza Evergreen",
     ingredients: "Looks fresh and green pizza",
     price: 249,
-    photoName: "pizzas/evergreen.jpg",
+    photoName: evePizza,
     soldOut: true,
   },
   {
     name: "Sweet and Salty",
     ingredients: "With onions and tomatoes and tangy masala with cheese on top",
     price: 199,
-    photoName: "pizzas/Corn.jpg",
+    photoName: cornImg,
     soldOut: false,
   },
 ];
